8bd80ddce92d775481297ace3f48d5d9e88e1149
Terri <404516+tpenn@users.noreply.github.com>
2023-06-25 08:10:46 -0500

    Completed some helper functions and partial validation.  I need to commit more frequently.

--- script.js
+++ script.js
@@ -4,13 +4,19 @@ window.addEventListener("load", function() {
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+       const planet = pickPlanet(listedPlanets);
+       addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
    })
    
+    const form = document.querySelector('[data-testid=testForm]');
+    form.addEventListener('submit', function() {
+        formSubmission(document, document.getElementById('faultyItems'), form.elements["pilotName"].value, form.elements['copilotName'].value, form.elements['fuelLevel'].value, form.elements['cargoMass'].value);
+    });
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -2,6 +2,17 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
+    document.getElementById('missionTarget').innerHTML = `
+    <h2>Mission Destination</h2>
+    <ol>
+        <li>Name: ${name}</li>
+        <li>Diameter: ${diameter}</li>
+        <li>Star: ${star}</li>
+        <li>Distance from Earth: ${distance}</li>
+        <li>Number of Moons: ${moons}</li>
+    </ol>
+    <img src="${imageUrl}">
+`;
    // Here is the HTML formatting for our mission target div.
    /*
                 <h2>Mission Destination</h2>
@@ -17,23 +28,36 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+    if (testInput === '') return 'Empty';
+    if (isNaN(+testInput)) return 'Not a Number';
+    return 'Is a Number';
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    const validate = [pilot, copilot, fuelLevel, cargoLevel].map(validateInput);
+    const correct  = ['Not a Number', 'Not a Number', 'Is a Number', 'Is a Number'];
+
+    console.log(validate);
+    if (validate.includes('Empty')) {
+        window.alert('All fields are required, please complete the entire form.');
+        event.preventDefault();
+    } else if (!validate.every((value, index) => value === correct[index])) {
+        window.alert('Please make sure to enter valid information for every field.');
+        event.preventDefault();
+    }
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json")
+        .then((response) => response.json());
 
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+    return planets[Math.floor(Math.random() * planets.length)];
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
